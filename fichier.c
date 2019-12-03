4de74b59851a7fdbd7cff3c3619538d5fc1d6310
FONTES Damien <f17019047@L-024110A003-06.salsa.univ-amu.fr>
2019-12-03 17:34:56 +0100

    Ecrire et lecture binaire

--- fichier.c
+++ fichier.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include "fichier.h"
 
-fichier ouvrir_fichier (char * nomF, char mode) {
+fichier ouvrir_fichier (char * nomF, char * mode) {
   fichier fic;
   fic = NULL;
   fic = fopen (nomF,mode);
+
   if (fic == NULL)
     printf("ERREUR : Fichier introuvable ou mode invalide\n");
   return fic;
 }
 
-void ecrire_binaire (fichier fic, int bit) {
-  fwrite ()
+void ecrire_binaire (fichier fic, char bit) {
+  fwrite (&bit,1,1,fic);
+  printf ("écriture de %c\n", bit);
+}
+
+void lire_binaire (fichier fic, int taille_buffer) {
+  char buffer[taille_buffer];
+  fread (buffer, sizeof(buffer[0]), sizeof(buffer), fic);
+
+  printf ("LIRE : %c\n", buffer[0]);
+  printf ("LIRE : %c\n", buffer[1]);
+  printf ("LIRE : %c\n", buffer[2]);
+  printf ("LIRE : %c\n", buffer[3]);
 }
 
 void fermer_fichier (fichier fic) {
   fclose(fic);
   printf("Fichier fermé\n");
+}
+
+void main () {
+  fichier ficW = ouvrir_fichier("test.bin", "wb");
+  ecrire_binaire (ficW, '1');
+  ecrire_binaire (ficW, '0');
+  ecrire_binaire (ficW, '1');
+  fermer_fichier(ficW);
+
+  fichier ficR = ouvrir_fichier("test.bin", "rb");
+  lire_binaire (ficR, 10);
+  fermer_fichier(ficR);
 }
